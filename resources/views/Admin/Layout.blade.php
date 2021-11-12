14451a768390e6dab0e2dc9ac09615d56a6b2788
vicky chhetri <specialpower321@gmail.com>
2021-11-12 23:38:23 +0530

    name and creatd info added to dasboard header:3

--- resources/views/Admin/Layout.blade.php
+++ resources/views/Admin/Layout.blade.php
@@ -292,7 +292,7 @@
               <img src="/Admin/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image" />
             </div>
             <div class="pull-left info">
-              <p>  {{session()->get('firstname') }}  {{session()->get('lastname') }}</p>
+              <p>  @{{session()->get('username') }} </p>
 
               <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
