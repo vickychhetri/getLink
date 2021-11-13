80a0ec7c883aa0e7baf832e53892b52121f2ff48
vicky chhetri <specialpower321@gmail.com>
2021-11-13 19:31:28 +0530

    Getlink.Page dashboard link

--- resources/views/Admin/Layout.blade.php
+++ resources/views/Admin/Layout.blade.php
@@ -43,7 +43,7 @@
     <div class="wrapper">
       <header class="main-header">
         <!-- Logo -->
-        <a href="index2.html" class="logo"><b>GetLink</b>.Page</a>
+        <a href="/dashboard" class="logo"><b>GetLink</b>.Page</a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top" role="navigation">
           <!-- Sidebar toggle button-->
