95323827396a901e2cea5f00d2dea83253579ea3
vicky chhetri <specialpower321@gmail.com>
2021-11-14 16:22:40 +0530

    userpage code added:8

--- resources/views/template/username.blade.php
+++ resources/views/template/username.blade.php
@@ -7,7 +7,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12">
-            {{$pageData->{{$pageData}}
+            {{$pageData->$content}}
         </div>
     </div>
 </div>
