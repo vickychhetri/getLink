2659e1a43b04f4c7cff14926f9751c83a40b3c6d
vicky chhetri <specialpower321@gmail.com>
2021-11-14 17:44:01 +0530

    edit 1.0

--- resources/views/template/username.blade.php
+++ resources/views/template/username.blade.php
@@ -7,7 +7,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12">
-            {{!! html_entity_decode($pageData[0]->content) !!}}
+            {{ html_entity_decode($pageData[0]->content) }}
         </div>
     </div>
 </div>
