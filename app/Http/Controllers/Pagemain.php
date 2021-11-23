42c2cd90804bb81a9749a398d933524ec2f4f962
vicky chhetri <specialpower321@gmail.com>
2021-11-23 22:51:10 +0530

    testing 2

--- app/Http/Controllers/Pagemain.php
+++ app/Http/Controllers/Pagemain.php
@@ -18,16 +18,15 @@ class Pagemain extends Controller
             ->where('userpagebases.userName', '=', $username)
             ->get()
             ->first();
-
-
+            echo $HFScrpts;
         $usernamePage = DB::table('pages')
         ->join('userpagebases', 'pages.userId', '=', 'userpagebases.id')
         ->select('pages.*', 'userpagebases.firstName','userpagebases.lastName','userpagebases.mobile','userpagebases.userName')
         ->where('userpagebases.userName', '=', $username)
         ->get();
-        return view('template.username')
-        ->with('pageData',$usernamePage)
-        ->with('$headers',$HFScrpts);
+        // return view('template.username')
+        // ->with('pageData',$usernamePage)
+        // ->with('$headers',$HFScrpts);
         
     }catch(Exception $e){
 
