53c44a933b9b32dfb9c5dd9f27524ca75cd25095
vicky chhetri <specialpower321@gmail.com>
2021-11-23 21:21:58 +0530

    remove tiny

--- resources/views/Admin/headerForm.blade.php
+++ resources/views/Admin/headerForm.blade.php
@@ -96,62 +96,63 @@
 @endsection
 
 @section('footercontainer')
-<script src="https://cdn.tiny.cloud/1/0nj8pmqfaosicobgrfr3hxgklq00xrjmm5awxk07vguu1qf5/tinymce/5/tinymce.min.js"
-    referrerpolicy="origin"></script>
+
+<!-- <script src="https://cdn.tiny.cloud/1/0nj8pmqfaosicobgrfr3hxgklq00xrjmm5awxk07vguu1qf5/tinymce/5/tinymce.min.js"
+    referrerpolicy="origin"></script> -->
 <script>
-tinymce.init({
-    selector: '#mytextareaboxadminsection',
-    plugins: 'a11ychecker advcode casechange export formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
-    toolbar: 'a11ycheck addcomment showcomments casechange checklist code export formatpainter pageembed permanentpen table',
-    toolbar_mode: 'floating',
-    tinycomments_mode: 'embedded',
-    tinycomments_author: 'Author name',
-    image_class_list: [{
-        title: 'img-fluid',
-        value: 'img-fluid'
-    },{
-        title: 'mx-auto',
-        value: 'mx-auto'
-    }, ],
-    height: 500,
-    setup: function(editor) {
-        editor.on('init change', function() {
-            editor.save();
-        });
-    },
-    plugins: [
-        "advlist autolink lists link image charmap print preview anchor",
-        "searchreplace visualblocks code fullscreen",
-        "insertdatetime media table contextmenu paste imagetools"
-    ],
-    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image ",
+// tinymce.init({
+//     selector: '#mytextareaboxadminsection',
+//     plugins: 'a11ychecker advcode casechange export formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+//     toolbar: 'a11ycheck addcomment showcomments casechange checklist code export formatpainter pageembed permanentpen table',
+//     toolbar_mode: 'floating',
+//     tinycomments_mode: 'embedded',
+//     tinycomments_author: 'Author name',
+//     image_class_list: [{
+//         title: 'img-fluid',
+//         value: 'img-fluid'
+//     },{
+//         title: 'mx-auto',
+//         value: 'mx-auto'
+//     }, ],
+//     height: 500,
+//     setup: function(editor) {
+//         editor.on('init change', function() {
+//             editor.save();
+//         });
+//     },
+//     plugins: [
+//         "advlist autolink lists link image charmap print preview anchor",
+//         "searchreplace visualblocks code fullscreen",
+//         "insertdatetime media table contextmenu paste imagetools"
+//     ],
+//     toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image ",
 
-    image_title: true,
-    automatic_uploads: true,
-    images_upload_url: '/upload',
-    file_picker_types: 'image',
-    file_picker_callback: function(cb, value, meta) {
-        var input = document.createElement('input');
-        input.setAttribute('type', 'file');
-        input.setAttribute('accept', 'image/*');
-        input.onchange = function() {
-            var file = this.files[0];
+//     image_title: true,
+//     automatic_uploads: true,
+//     images_upload_url: '/upload',
+//     file_picker_types: 'image',
+//     file_picker_callback: function(cb, value, meta) {
+//         var input = document.createElement('input');
+//         input.setAttribute('type', 'file');
+//         input.setAttribute('accept', 'image/*');
+//         input.onchange = function() {
+//             var file = this.files[0];
 
-            var reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onload = function() {
-                var id = 'blobid' + (new Date()).getTime();
-                var blobCache = tinymce.activeEditor.editorUpload.blobCache;
-                var base64 = reader.result.split(',')[1];
-                var blobInfo = blobCache.create(id, file, base64);
-                blobCache.add(blobInfo);
-                cb(blobInfo.blobUri(), {
-                    title: file.name
-                });
-            };
-        };
-        input.click();
-    }
-});
+//             var reader = new FileReader();
+//             reader.readAsDataURL(file);
+//             reader.onload = function() {
+//                 var id = 'blobid' + (new Date()).getTime();
+//                 var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+//                 var base64 = reader.result.split(',')[1];
+//                 var blobInfo = blobCache.create(id, file, base64);
+//                 blobCache.add(blobInfo);
+//                 cb(blobInfo.blobUri(), {
+//                     title: file.name
+//                 });
+//             };
+//         };
+//         input.click();
+//     }
+// });
 </script>
 @endsection
