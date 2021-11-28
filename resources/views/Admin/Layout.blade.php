6bce7a60d4cd75b17541a734b0d719ea1b00bdec
vicky chhetri <specialpower321@gmail.com>
2021-11-28 15:15:34 +0530

    remove friends follower

--- resources/views/Admin/Layout.blade.php
+++ resources/views/Admin/Layout.blade.php
@@ -78,7 +78,7 @@
                     </p>
                   </li>
                   <!-- Menu Body -->
-                  <li class="user-body">
+                  <!-- <li class="user-body">
                     <div class="col-xs-4 text-center">
                       <a href="#">Followers</a>
                     </div>
@@ -88,7 +88,7 @@
                     <div class="col-xs-4 text-center">
                       <a href="#">Friends</a>
                     </div>
-                  </li>
+                  </li> -->
                   <!-- Menu Footer-->
                   <li class="user-footer">
                     <div class="pull-left">
