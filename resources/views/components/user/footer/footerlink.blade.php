2e264d98f5750641c2640fe97e8a2f341432251a
vicky chhetri <specialpower321@gmail.com>
2022-02-18 20:21:57 +0530

    footer color change

--- resources/views/components/user/footer/footerlink.blade.php
+++ resources/views/components/user/footer/footerlink.blade.php
@@ -1,9 +1,9 @@
-<footer class="footer" style="background-color:#00080a;">
+<footer class="footer">
     <div class="container">
         <div class="row">
 
             <div class="col-lg-12 col-12">
-                <p class="copyright-text text-center" style="color: white;">Copyright &copy; 2021 urprofile.in . All rights reserved</p>
+                <p class="copyright-text text-center" style="color: black;">Copyright &copy; 2021 urprofile.in . All rights reserved</p>
             </div>
 
         </div>
