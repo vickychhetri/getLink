942e505ff881e9a818d1c2c3a077b30c9282722c
vicky chhetri <specialpower321@gmail.com>
2022-02-19 11:23:09 +0530

    validate image

--- app/Http/Controllers/Profileupdate.php
+++ app/Http/Controllers/Profileupdate.php
@@ -25,7 +25,7 @@ class Profileupdate extends Controller
         $images=Imagecollection::where('id','=',$id)->get()->first();
         
         if($userID==$images->userId){
-            unlink("/storage/".$images->images);
+            unlink(storage_path($images->images));
             $user = Imagecollection::find($id); $user->delete();
             
         }
