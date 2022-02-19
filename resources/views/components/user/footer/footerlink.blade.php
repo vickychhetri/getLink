a0c4b2d150a8d3bf8ce5b659abbface663ec9c7b
vicky chhetri <specialpower321@gmail.com>
2022-02-19 14:07:39 +0530

    validate image

--- resources/views/components/user/footer/footerlink.blade.php
+++ resources/views/components/user/footer/footerlink.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
 
             <div class="col-lg-12 col-12">
-                <p class="copyright-text text-center" style="color: black;">Copyright &copy; 2021 urprofile.in . All rights reserved</p>
+                <p class="copyright-text text-center" style="color: black;">Copyright &copy; 2022 urprofile.in . All rights reserved</p>
             </div>
 
         </div>
