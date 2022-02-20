516382ec546544998e973ce1fc9840bc0039bc5b
vicky chhetri <specialpower321@gmail.com>
2022-02-20 09:28:20 +0530

    mail setting

--- resources/views/Guest/mail.blade.php
+++ resources/views/Guest/mail.blade.php
@@ -5,10 +5,10 @@
 </head>
 <body>
     <h1>{{ $details['title'] }}</h1>
-    <p>{{ $details['body'] }}</p> <br/>
-    <p>First Name: {{ $details['firstname'] }}</p><br/>
-    <p>Last Name {{ $details['lastname'] }}</p><br/>
-    <p>Email: {{ $details['email'] }}</p><br/>
+    <p>{{ $details['body'] }}</p>  
+    <p>First Name: {{ $details['firstname'] }}</p> 
+    <p>Last Name {{ $details['lastname'] }}</p> 
+    <p>Email: {{ $details['email'] }}</p> 
     <p>Mobile: {{ $details['mobile'] }}</p><br/><br/>
     <strong> 
     <p>Username(login): {{ $details['username'] }}</p>
@@ -18,15 +18,17 @@
    
     <p>Thank you</p>
     <br/>
-    <br/> <b>
-    --
-Regards:
-
-Vicky Chhetri
-Software Engineer
-<a href="https://urprofile.in/"> www.urprofile.in </a>
-Email: <a>admin@urprofile.in </a>
-Help Desk: <a href="https://helpdesk.urprofile.in/">helpdesk.urprofile.in</a>
+    <br/> 
+    
+    <b>
+<p>     -- </p>
+<p>Regards:</p>
+<br/>
+<p>Vicky Chhetri</p>
+<p>Software Engineer</p>
+<p><a href="https://urprofile.in/"> www.urprofile.in </a></p>
+<p>Email: <a>admin@urprofile.in </a></p>
+<p>Help Desk: <a href="https://helpdesk.urprofile.in/">helpdesk.urprofile.in</a> </p>
 </b>
 </body>
 </html>
